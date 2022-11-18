a88fcf1fcbb176a6e11647800c091a23df09fb24
garlicGrape <sanchit809@gmail.com>
2022-11-17 21:25:21 -0500

    Saving work for employee model. Added the private member variables and 3 different constructors

--- src/main/java/com/revature/P1SanchitKumar/Models/Employee.java
+++ src/main/java/com/revature/P1SanchitKumar/Models/Employee.java
@@ -1,4 +1,35 @@
 package com.revature.P1SanchitKumar.Models;
 
 public class Employee {
+
+    private String employee_username;
+
+    private String employee_email;
+
+    private String employee_password;
+
+    private String employee_name;
+
+    private int employee_role;
+
+    public Employee() {
+
+    }
+
+    public Employee(String employee_username, String employee_email, String employee_password, String employee_name) {
+        this.employee_username = employee_username;
+        this.employee_email = employee_email;
+        this.employee_password = employee_password;
+        this.employee_name = employee_name;
+        this.employee_role = 0;
+    }
+
+    public Employee(String employee_username, String employee_email, String employee_password, String employee_name, int employee_role) {
+        this.employee_username = employee_username;
+        this.employee_email = employee_email;
+        this.employee_password = employee_password;
+        this.employee_name = employee_name;
+        this.employee_role = employee_role;
+
+    }
 }
