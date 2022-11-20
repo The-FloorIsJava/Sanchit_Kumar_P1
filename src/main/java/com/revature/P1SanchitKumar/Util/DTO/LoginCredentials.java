bd9d817daf1a805be0ce9fa695d20e14f77362ad
garlicGrape <sanchit809@gmail.com>
2022-11-20 10:55:21 -0500

    Preliminary work for RequestsService and DTO login credentials

---
+++ src/main/java/com/revature/P1SanchitKumar/Util/DTO/LoginCredentials.java
@@ -0,0 +1,24 @@
+package com.revature.P1SanchitKumar.Util.DTO;
+
+public class LoginCredentials {
+
+    private String employee_username;
+
+    private String employee_password;
+
+    public String getEmployee_username() {
+        return employee_username;
+    }
+
+    public void setEmployee_username(String employee_username) {
+        this.employee_username = employee_username;
+    }
+
+    public String getEmployee_password() {
+        return employee_password;
+    }
+
+    public void setEmployee_password(String employee_password) {
+        this.employee_password = employee_password;
+    }
+}
