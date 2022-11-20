3d31a04561a18227e417f0c9909d0526a347cb13
garlicGrape <sanchit809@gmail.com>
2022-11-19 20:37:54 -0500

    Created the ConnectionFactory.java file to handle connections to the postgresql database

---
+++ src/main/java/com/revature/P1SanchitKumar/Util/ConnectionFactory.java
@@ -0,0 +1,47 @@
+package com.revature.P1SanchitKumar.Util;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionFactory {
+
+    private static final ConnectionFactory connectionFactory = new ConnectionFactory(); //using eager instantiation
+
+    private Properties properties = new Properties();
+
+    private ConnectionFactory() {
+        try {
+            properties.load(new FileReader("src/main/resources/db.properties"));
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ConnectionFactory getConnectionFactory(){
+        return connectionFactory;
+    }
+
+    public Connection getConnection(){
+        try {
+            return DriverManager.getConnection(properties.getProperty("url"), properties.getProperty("username"), properties.getProperty("password"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
