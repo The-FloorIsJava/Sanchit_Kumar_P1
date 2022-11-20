39dce7c3cc7a1a6501470a010dc36b45716d602d
garlicGrape <sanchit809@gmail.com>
2022-11-20 11:14:18 -0500

    Added the RequestsController file

--- src/main/java/com/revature/P1SanchitKumar/Controller/RequestsController.java
+++ src/main/java/com/revature/P1SanchitKumar/Controller/RequestsController.java
@@ -1,4 +1,28 @@
 package com.revature.P1SanchitKumar.Controller;
 
+import com.revature.P1SanchitKumar.DAO.RequestsDAO;
+import com.revature.P1SanchitKumar.Models.Requests;
+import com.revature.P1SanchitKumar.Service.RequestsService;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
 public class RequestsController {
+
+    RequestsService requestsService;
+
+    Javalin app;
+
+    public RequestsController(Javalin app) {
+        requestsService = new RequestsService (new RequestsDAO());
+        this.app = app;
+    }
+
+    public void requestsEndpoint() {
+
+    }
+
+    private void getSpecificRequestsHandler(Context context) {
+        String employee_username = context.pathParam("employee_username");
+        Requests employee = requestsService.getRequestsByUser(employee_username); // get back to this
+    }
 }
